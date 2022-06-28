af707826d95a261ade747c90a7cf893f583012cf
fatihalii <68364898+fatihalii@users.noreply.github.com>
2022-06-28 17:55:59 +0300

    Add files via upload

---
+++ Controllers/SchoolController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        public ActionResult Index()
+        {
+            IEnumerable<Student> students = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44361/api/");
+                var responseTask = client.GetAsync("student");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+
+                if(result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Student>>();
+                    readTask.Wait();
+
+
+                    students = readTask.Result;
+                }
+
+                else
+                {
+                    students = Enumerable.Empty<Student>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+
+            }
+
+
+
+
+            return View(students);
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Final.Controllers
+{
+    public class StudentController : ApiController
+    {
+
+        public IHttpActionResult GetAllStudents()
+        {
+            var students = new List<Student>();
+
+            try
+            {
+                using (var context = new SchoolDBEntities())
+                {
+                    students = context.Students.ToList();
+                    if (students.Count > 0)
+                    {
+                        return Ok(students);
+                    }
+                    else
+                    {
+                        return NotFound();
+
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+        }
+
+        public IHttpActionResult PostNewStudent(Student student)
+        {
+         
+            if(!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+                using(var context = new SchoolDBEntities())
+                {
+                    context.Students.Add(student);
+                    var numberOfInserted = context.SaveChanges();
+                    if (numberOfInserted > 0)
+                    {
+                        return Ok(student);  
+                    }
+                    else { return BadRequest(); }
+                }
+            
+          
+        }
+
+        public IHttpActionResult PutUpdateStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+            using (var context = new SchoolDBEntities())
+            {
+                var existingStudent = context.Students.Where(s => s.Id == student.Id).FirstOrDefault<Student>();
+
+                if(existingStudent != null)
+                {
+                    existingStudent.Name = student.Name;
+                    var numberOfInserted = context.SaveChanges();
+                    if (numberOfInserted > 0)
+                    {
+                        return Ok(student);
+                    }
+                    else { return BadRequest(); }
+
+                }
+            }
+            return null;
+        }
+
+
+        public IHttpActionResult DeleteStudent(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Not a valid student id");
+
+            using(var context = new SchoolDBEntities())
+            {
+                var student = context.Students.FirstOrDefault(x => x.Id == id);
+                if(student != null)
+                {
+                    context.Students.Remove(student);
+                    var numberOfDeleted = context.SaveChanges();
+                    if (numberOfDeleted > 0)
+                        return Ok();
+
+
+                }
+                return BadRequest();
+            }
+        }
+
+
+    }
+}
